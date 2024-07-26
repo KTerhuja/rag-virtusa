93649d58573b6888c891786a7e71bbb7b0ee4c50
KTerhuja-2 <142892336+KTerhuja-2@users.noreply.github.com>
2024-07-26 11:36:59 +0000

    updated code

--- create_rag.py
+++ create_rag.py
@@ -1,3 +1,4 @@
+# Importing necessary modules and functions from different components
 from agents import agents
 from chunk import splitter
 from document_loader import loader
@@ -7,21 +8,26 @@ from retriever import retriever
 from vector_store import create_vector_store
 from tools import tools
 
-
+# Load the document pdf/text/folder of docs
 docs = loader.load_docs('data/sample_doc.pdf', type='pdf')
 
+# Split the loaded document into chunks based on character/tokens
 split_texts = splitter.split(docs, 'character')
 
+# Create an embedded vector store from the split texts using the openai/gemini embedding type
 embedded_vector_store = create_vector_store.create_vector_store(split_texts, embedding_type='gemini')
 
+# Define the input query for retrieval
 query = '# input query'
 
+# Retrieve relevant context from the embedded vector store based on the query
 retrieve_context = retriever.retrive(embedded_vector_store, query)
 
+# Define the prompt for the language model
 prompt = 'prompt'
 
+# Load the language model of type 'gemini'/openai
 llm = load_model.load_llm(type='gemini')
 
-
-
-
+# Invoke the language model with the combined prompt and query to get the output
+output = llm.invoke(prompt + query)
